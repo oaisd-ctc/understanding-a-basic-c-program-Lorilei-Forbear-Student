896c6bdc10972239ca1e6a8276862cf711167391
Lorilei-Forbear-Student <207830@student.oaisd.org>
2024-10-07 13:11:13 +0000

    added c# file and painstakingly typed out code.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Lorilei Forbear.");
+
+            
+            int age = 16;
+            string greeting = "Hello World";
+            string firstname = "Lorilei";
+            string lastname = "Forbear";
+            double money = 2.79;
+
+            Console.WriteLine(greeting + "!" + "My name is " + firstname + " " + lastname ". I am " age + "years old. I just spent $" money + "on an energy drink.");
+        }
+}
